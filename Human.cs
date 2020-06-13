a9af91d21ad56e0a2bbe4b7022dbbcbcc3931b19
eniepa <apeine.k@gmail.com>
2020-06-13 17:44:37 +0300

    Add project files.

---
+++ Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppLearning
+{
+    class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Human()
+        {
+
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("hi, my full name is " + this.Name + " " + this.Surname
+                                 + ". " + "I am " + this.Age + " years old.");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAppLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human person = new Human();
+
+            person.Name = "Jennifer";
+            person.Surname = "Lopez";
+            person.Age = 25;
+
+            person.Introduce();
+
+            person.Name = "Anna";
+            person.Introduce();
+        }
+    }
+
+   
+}
